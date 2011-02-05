15837be8a926fc42cde61798962424361b4a99b9
Tom Stuart <tom@experthuman.com>
2011-02-05 12:06:03 +0000

    Initial commit of fresh Rails 3.0.3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapybaraEnvjsButtonBug::Application.config.secret_token = '6fae22b1b357b3ebf5a54bd30e108d58afd5e2b13109428eb0696a4f2cfd7e049264f96d53ccb6353f2b5f701465f3eb66f3f8d60f90c55fc177a76bae3be827'
